b175e02b331f2b5a65791909b57eee2a528e7a9d
Pollyany Carvalho <kerleypollyany@gmail.com>
2026-08-29 12:32:15 -0300

    feat: implement user routes and controller

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
 import { errorHandling } from "./middlewares/error-handling";
+import { routes } from "./routes";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(routes);
 
 app.use(errorHandling);
 
---
+++ src/controllers/users-controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+class UsersController {
+  async create(request: Request, response: Response) {
+    response.json({ message: "ok" });
+  }
+}
+
+export { UsersController };
