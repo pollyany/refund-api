7fb0aaaadadcc3b04a84e4671484b5fb96de4a6d
Pollyany Carvalho <kerleypollyany@gmail.com>
2026-08-29 19:08:41 -0300

    feat: implement disk storage for file handling and add validation in uploads controller

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -6,7 +6,8 @@ const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp");
 
 const UPLOADS_FOLDER = path.relative(TMP_FOLDER, "uploads");
 
-const MAX_FILE_SIZE = 1024 * 1024 * 3; // 3MB
+const MAX_SIZE = 3;
+const MAX_FILE_SIZE = 1024 * 1024 * MAX_SIZE; // 3MB
 const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png"];
 
 const MULTER = {
@@ -26,4 +27,5 @@ export default {
   MAX_FILE_SIZE,
   ACCEPTED_IMAGE_TYPES,
   MULTER,
+  MAX_SIZE,
 };
--- src/controllers/uploads-controller.ts
+++ src/controllers/uploads-controller.ts
@@ -1,19 +1,51 @@
 import { Request, Response } from "express";
 import { AppError } from "@/utils/AppError";
-
+import uploadConfig from "@/config/upload";
+import z, { ZodError } from "zod";
+import { DiskStorage } from "@/providers/disk-storage";
 
 class UploadsController {
-    async create(request: Request, response: Response) {
-        if (!request.file) {
-            throw new AppError("File is required", 400);
+  async create(request: Request, response: Response) {
+    const diskStorage = new DiskStorage();
+
+    try {
+      const fileSchema = z
+        .object({
+          filename: z.string().min(1, "Arquivo é obrigatório"),
+          mimetype: z
+            .string()
+            .refine(
+              (type) => uploadConfig.ACCEPTED_IMAGE_TYPES.includes(type),
+              {
+                message: "Formato de arquivo inválido",
+              },
+            ),
+          size: z
+            .number()
+            .positive()
+            .refine(
+              (size) => size <= uploadConfig.MAX_FILE_SIZE,
+              `Arquivo excede o tamanho máximo permitido de ${uploadConfig.MAX_SIZE}MB`,
+            ),
+        })
+        .passthrough();
+
+      const file = fileSchema.parse(request.file);
+
+      const fileName = await diskStorage.saveFile(file.filename);
+
+      return response.json({ fileName });
+    } catch (error) {
+      if (error instanceof ZodError) {
+        if (request.file) {
+          await diskStorage.deleteFile(request.file.filename, "tmp");
         }
 
-        const { filename } = request.file;
-
-        return response.status(201).json({
-            filename,
-        });
+        throw new AppError(error.issues[0].message);
+      }
+      throw error;
     }
+  }
 }
 
 export { UploadsController };
