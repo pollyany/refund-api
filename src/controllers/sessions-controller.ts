47c3d22923fb0eaecad5d30d925d2f666fb57b42
Pollyany Carvalho <kerleypollyany@gmail.com>
2026-08-29 13:38:12 -0300

    feat: add sessions controller and routes for user authentication

---
+++ src/controllers/sessions-controller.ts
@@ -0,0 +1,36 @@
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
+import { compare } from "bcrypt";
+import { Request, Response } from "express";
+import z from "zod";
+
+class SessionsController {
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      email: z.string().email({ message: "E-mail inválido" }),
+      password: z.string(),
+    });
+
+    const { email, password } = bodySchema.parse(request.body);
+
+    const user = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      throw new AppError("E-mail ou senha incorretos", 401);
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError("E-mail ou senha incorretos", 401);
+    }
+
+    response.json({ message: "Session created successfully" });
+  }
+}
+
+export { SessionsController };
