dad2b913b20d9e21f0a2ff35ca694674cea8b286
Pollyany Carvalho <kerleypollyany@gmail.com>
2026-08-29 13:18:43 -0300

    feat: add user registration with password hashing and validation

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,8 +1,47 @@
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
+import { UserRole } from "@prisma/client";
+import { hash } from "bcrypt";
 import { Request, Response } from "express";
+import z from "zod";
 
 class UsersController {
   async create(request: Request, response: Response) {
-    response.json({ message: "ok" });
+    const bodySchema = z.object({
+      name: z.string().trim().min(2, { message: "Nome é obrigatório" }),
+      email: z
+        .string()
+        .trim()
+        .email({ message: "E-mail inválido" })
+        .toLowerCase(),
+      password: z
+        .string()
+        .min(6, { message: "A senha deve ter pelo menos 6 dígitos" }),
+      role: z
+        .enum([UserRole.employe, UserRole.manager])
+        .default(UserRole.employe),
+    });
+
+    const { email, name, password, role } = bodySchema.parse(request.body);
+
+    const userWithSameEmail = await prisma.user.findFirst({ where: { email } });
+
+    if (userWithSameEmail) {
+      throw new AppError("Já existe um usuário cadastrado com esse e-mail");
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        role,
+      },
+    });
+
+    response.status(201).json();
   }
 }
 
